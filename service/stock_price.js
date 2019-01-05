479e1161a79eff23551acbf55e3a8525f127f2bf
David Söderberg <davidsoderberg@comhem.se>
2019-01-05 15:39:38 +0100

    new api for stocks and temporary also added api with delay

--- service/stock_price.js
+++ service/stock_price.js
@@ -1,14 +1,48 @@
-const Stocks = require('stocks.js');
+// const Stocks = require('stocks.js');
 
-const stocks = new Stocks({ apiKey: process.env.apiKey });
-const INTERVAL = '1min';
-const AMOUNT = 1;
+// const stocks = new Stocks({ apiKey: process.env.apiKey });
+// const INTERVAL = '1min';
+// const AMOUNT = 1;
 
-exports.realTimeSharePrice = async (ticker) => {
+/* exports.realTimeSharePrice = async (ticker) => {
   const stock = await stocks.timeSeries({
     symbol: ticker,
     interval: INTERVAL,
     amount: AMOUNT,
   });
   return stock[0].close;
-};
+}; */
+const request = require('request');
+const debug = require('debug')('b-rscase:stock_price');
+const chalk = require('chalk');
+
+const API_KEY = process.env.DATA_KEY;
+
+exports.realTime = symbols => new Promise((resolve, reject) => {
+  const symbolsString = symbols.join();
+  const URI = `https://www.worldtradingdata.com/api/v1/stock?symbol=${symbolsString}&api_token=${API_KEY}`;
+  request({ url: URI, json: true }, (error, response, body) => {
+    if (error) {
+      debug(chalk.red(error));
+      reject(error);
+    }
+    if (!body.data) {
+      reject(new Error('Invalid symbols'));
+    }
+    resolve(body);
+  });
+});
+
+exports.delay = symbol => new Promise((resolve, reject) => {
+  request({ url: process.env.URL_API + symbol, json: true }, (error, response, body) => {
+    if (error) {
+      reject(error);
+    } else {
+      if (body.totalNumberOfHits === 0) {
+        reject(new Error('Invalid symbol'));
+      }
+      const stock = body.hits[0].topHits[0];
+      resolve(stock);
+    }
+  });
+});
